1c9fa75959654a3d4605f6eb96dd5deae6272a98
ElfenCamii <camiilo.pinto@gmail.com>
2022-08-25 15:50:18 -0500

    Moda ya en funcion

--- moda.js
+++ moda.js
@@ -1,33 +1,64 @@
-const lista1 = [
-    1,
-    2,
-    3,
-    1,
-    2,
-    3,
-    4,
-    2,
-    2,
-    2,
-    1,
-];
+function calcularModa(datos){
+    console.log("chango", datos);
 
-const lista1Count = {};
+    const datosCount = {};
 
-lista1.map(
-    function(elemento){
-        if (lista1Count [elemento]){
-            lista1Count [elemento] += 1;
-        }else{
-            lista1Count [elemento] =  1;
+    datos.map(
+        function(elemento){
+            if (datosCount [elemento]){
+                datosCount [elemento] += 1;
+            }else{
+                datosCount [elemento] =  1;
+            }
         }
-    }
-);
+    );
 
-const lista1Array = Object.entries(lista1Count).sort(
-    function(elementoA, elementoB){
-        return elementoA[1] - elementoB[1];
-    }
-);
+    let moda;
+    
+    const datosArray = Object.entries(datosCount).sort(
+        function(elementoA, elementoB){
+            return elementoA[1] - elementoB[1];
+        }
+    );
+    
+    const datosmoda = datosArray[datosArray.length - 1];
+    
+    moda = datosmoda;
 
-const moda = lista1Array[lista1Array.length - 1];
+    return moda;
+
+}
+
+// const lista1 = [
+//     1,
+//     2,
+//     3,
+//     1,
+//     2,
+//     3,
+//     4,
+//     2,
+//     2,
+//     2,
+//     1,
+// ];
+
+// const lista1Count = {};
+
+// lista1.map(
+//     function(elemento){
+//         if (lista1Count [elemento]){
+//             lista1Count [elemento] += 1;
+//         }else{
+//             lista1Count [elemento] =  1;
+//         }
+//     }
+// );
+
+// const lista1Array = Object.entries(lista1Count).sort(
+//     function(elementoA, elementoB){
+//         return elementoA[1] - elementoB[1];
+//     }
+// );
+
+// const moda = lista1Array[lista1Array.length - 1];
